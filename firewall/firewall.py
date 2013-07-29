5577c2babaa4521e1de5dfa09d6400c2515f16bc
Gowri Shankar <gowrishankar_m@hotmail.com>
2013-07-29 19:09:07 +0530

    Firewall assignment final.

--- firewall/firewall.py
+++ firewall/firewall.py
@@ -31,27 +31,53 @@ class Firewall (EventMixin):
     def __init__ (self):
         self.listenTo(core.openflow)
         log.debug("Enabling Firewall Module")
+        self.deny = []
+        with open(policyFile, "r") as csvfile:
+            dictReader = csv.DictReader(csvfile)
+            for connectionPair in dictReader:
+                self.deny.append((EthAddr(connectionPair['mac_0']),EthAddr(connectionPair['mac_1'])))
+                self.deny.append((EthAddr(connectionPair['mac_1']),EthAddr(connectionPair['mac_0'])))
 
     def _handle_ConnectionUp (self, event):    
         ''' Add your logic here ... '''
-        print "Firewall connection up called"
-        with open(policyFile, "r") as csvfile:
-            dictReader = csv.DictReader(csvfile)
-
-            for a in dictReader:
-                print "hello---->" + str(a)
-                msg = of.ofp_flow_mod()
-                msg.priority = 65535
-                msg.match.dl_src = EthAddr(a['mac_0'])
-                msg.match.dl_dst = EthAddr(a['mac_1'])
-                self.connection.send(msg)
-        
-
+        for (src, dst) in self.deny:
+            match = of.ofp_match()
+            match.dl_src = src
+            match.dl_dst = dst
+            msg = of.ofp_flow_mod()
+            msg.match = match
+            event.connection.send(msg)
     
         log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
-
+        
 def launch ():
     '''
     Starting the Firewall module
     '''
     core.registerNew(Firewall)
+    
+    
+from pyretic.lib.corelib import *
+from pyretic.lib.std import *
+ 
+from pyretic.modules.mac_learner import mac_learner as act_like_switch
+ 
+import csv, os
+policy_file = "%s/pyretic/pyretic/examples/firewall-policies.csv" % os.environ[ 'HOME' ]
+ 
+def main():
+    # start with a policy that doesn't match any packets
+    not_allowed = none
+    # and add traffic that isn't allowed
+    with open(policy_file, 'rb') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            not_allowed = not_allowed + match(srcmac=MAC(row['mac_0']), dstmac=MAC(row['mac_1'])) + match(srcmac=MAC(row['mac_1']), dstmac=MAC(row['mac_0']))
+ 
+    # express allowed traffic in terms of not_allowed - hint use '~'
+    allowed = ~not_allowed
+#    allowed = if_(not_allowed, drop, passthrough)
+ 
+    # and only send allowed traffic to the mac learning (act_like_switch) logic
+    return allowed >> act_like_switch()
+    
